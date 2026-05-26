92f14100d0f5dc9999a69230786766dd587fbf25
Emmanuel Durotimi <90837486+Durotemy@users.noreply.github.com>
2026-05-26 12:59:13 +0100

    picking up this old app and doing a refactor

--- app/index.tsx
+++ app/index.tsx
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <NavigationIndependentTree>
       <NavigationContainer>
-        {user ? <DrawerNavigation /> : <AuthNavigator />}
+        {!user ? <DrawerNavigation /> : <AuthNavigator />}
       </NavigationContainer>
     </NavigationIndependentTree>
   );
